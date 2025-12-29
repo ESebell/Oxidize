442cb4c79737ad3c4dd6d0a9f8c3846c2db29f64
ESebell <esebell@users.noreply.github.com>
2025-12-29 23:56:01 +0100

    Deploy with model listing

--- oxidize-38b2577a9c3347e9.js
+++ oxidize-38b2577a9c3347e9.js
@@ -241,20 +241,20 @@ function __wasm_bindgen_func_elem_4998(arg0, arg1, arg2) {
     wasm.__wasm_bindgen_func_elem_4998(arg0, arg1, addHeapObject(arg2));
 }
 
-function __wasm_bindgen_func_elem_3006(arg0, arg1) {
-    wasm.__wasm_bindgen_func_elem_3006(arg0, arg1);
-}
-
-function __wasm_bindgen_func_elem_2471(arg0, arg1) {
-    wasm.__wasm_bindgen_func_elem_2471(arg0, arg1);
+function __wasm_bindgen_func_elem_3007(arg0, arg1, arg2) {
+    wasm.__wasm_bindgen_func_elem_3007(arg0, arg1, addHeapObject(arg2));
 }
 
 function __wasm_bindgen_func_elem_987(arg0, arg1, arg2) {
     wasm.__wasm_bindgen_func_elem_987(arg0, arg1, addHeapObject(arg2));
 }
 
-function __wasm_bindgen_func_elem_3007(arg0, arg1, arg2) {
-    wasm.__wasm_bindgen_func_elem_3007(arg0, arg1, addHeapObject(arg2));
+function __wasm_bindgen_func_elem_2471(arg0, arg1) {
+    wasm.__wasm_bindgen_func_elem_2471(arg0, arg1);
+}
+
+function __wasm_bindgen_func_elem_3006(arg0, arg1) {
+    wasm.__wasm_bindgen_func_elem_3006(arg0, arg1);
 }
 
 function __wasm_bindgen_func_elem_5309(arg0, arg1, arg2, arg3) {
