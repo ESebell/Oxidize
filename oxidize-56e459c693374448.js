f70d19c279753130e5d2155770e12c813f2aec8b
ESebell <esebell@users.noreply.github.com>
2026-02-19 22:08:39 +0100

    Deploy v45: fix safe-area-inset-top for PWA

--- oxidize-56e459c693374448.js
+++ oxidize-56e459c693374448.js
@@ -237,12 +237,8 @@ if (!('encodeInto' in cachedTextEncoder)) {
 
 let WASM_VECTOR_LEN = 0;
 
-function __wasm_bindgen_func_elem_944(arg0, arg1, arg2) {
-    wasm.__wasm_bindgen_func_elem_944(arg0, arg1, addHeapObject(arg2));
-}
-
-function __wasm_bindgen_func_elem_2949(arg0, arg1, arg2) {
-    wasm.__wasm_bindgen_func_elem_2949(arg0, arg1, addHeapObject(arg2));
+function __wasm_bindgen_func_elem_4943(arg0, arg1, arg2) {
+    wasm.__wasm_bindgen_func_elem_4943(arg0, arg1, addHeapObject(arg2));
 }
 
 function __wasm_bindgen_func_elem_2948(arg0, arg1) {
@@ -253,8 +249,12 @@ function __wasm_bindgen_func_elem_2412(arg0, arg1) {
     wasm.__wasm_bindgen_func_elem_2412(arg0, arg1);
 }
 
-function __wasm_bindgen_func_elem_4943(arg0, arg1, arg2) {
-    wasm.__wasm_bindgen_func_elem_4943(arg0, arg1, addHeapObject(arg2));
+function __wasm_bindgen_func_elem_944(arg0, arg1, arg2) {
+    wasm.__wasm_bindgen_func_elem_944(arg0, arg1, addHeapObject(arg2));
+}
+
+function __wasm_bindgen_func_elem_2949(arg0, arg1, arg2) {
+    wasm.__wasm_bindgen_func_elem_2949(arg0, arg1, addHeapObject(arg2));
 }
 
 function __wasm_bindgen_func_elem_5254(arg0, arg1, arg2, arg3) {
